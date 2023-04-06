052ca2cf1f07d6e795015bc854e597575467c191
svolachugin <svolachugin@mail.ru>
2023-04-06 13:10:44 +0300

    изменен тип продаж с int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {500, 1000, 2000};
+        long[] sales = {500, 1000, 2000};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println(salesManager.max());
 
