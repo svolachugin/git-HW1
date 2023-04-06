dcd8871a0eaf4313268c7fc9d24f3a67beb547f5
svolachugin <svolachugin@mail.ru>
2023-04-06 16:13:07 +0300

    добавлен расчет обрезанного среднего

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {500, 1000, 2000};
+        int[] sales = {500, 1000, 3, 5000, 1, 0, 0, 2000};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println(salesManager.max());
+        System.out.println(salesManager.min());
+        System.out.println(salesManager.sum());
+        System.out.println(salesManager.cutAverage());
 
 
     }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,34 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+        }
+        return sum;
+    }
+
+
+    public int cutAverage() {
+        SalesManager salesManager = new SalesManager(sales);
+        int cutAverage = (salesManager.sum() - salesManager.max() - salesManager.min()) / (sales.length - 2);
+        return cutAverage;
+    }
+
 }
+
+
+
+
